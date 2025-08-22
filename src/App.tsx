cb62ed20fdc1f78adbd3bf40a73b5a0621e3433a
Elvira Krutova <ninelostr@gmail.com>
2025-08-22 22:55:49 +0100

    feat: add ModalShell component with accessibility features (plus ESC key and outside click to close)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { ModalShell } from './components/ModalShell/ModalShell';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <button type="button" onClick={() => setIsOpen(true)}>
+        Open uncontrolled form
+      </button>
+      <button type="button" onClick={() => setIsOpen(true)}>
+        Open React form
+      </button>
+
+      {isOpen &&
+        createPortal(
+          <ModalShell
+            id="shared-modal"
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+          />,
+          document.body
+        )}
+    </main>
+  );
 }
 
-export default App
+export default App;
